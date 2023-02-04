c98f319a05be2896624623f5d47c390867fa39b5
Fewatts <feralveswatts@gmail.com>
2023-02-04 16:15:44 -0300

    Notas no programa

--- Teste__/Java/src/FirstProgram.java
+++ Teste__/Java/src/FirstProgram.java
@@ -3,5 +3,10 @@ class FirstProgram {
         System.out.println("Hello world! This is my first of many\nprograms writen in Java by fernando A.!\nI will write more than");
         System.out.println(12323 * 5);
         System.out.println("Programs ! ");
+
+        // comentários -> úteis para deixar notas no código ou até mesmo observações
+        // Entry point -> ponto que todos os sistemas procuram para iniciar o projeto
+        // Métodos: System.ouy.println()
+        // Propriedades e variáveis -> Lugar que armazena dados para manipulação
     }
 }
