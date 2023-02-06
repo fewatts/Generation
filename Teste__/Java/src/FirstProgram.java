ec00bb664d8d729e87e46e7a77a2a2d5cfa779a9
Fewatts <feralveswatts@gmail.com>
2023-02-06 16:57:02 -0300

    Update ex

--- Teste__/Java/src/FirstProgram.java
+++ Teste__/Java/src/FirstProgram.java
@@ -1,6 +1,6 @@
 class FirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello world! This is my first of many\nprograms writen in Java by fernando A.!\nI will write more than");
+        System.out.println("Hello world! This is my first of many\nprograms writen in Java.\nI will write more than");
         System.out.println(12323 * 5);
         System.out.println("Programs ! ");
 
